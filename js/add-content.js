b30b97ba65b4f37bfbecadc3ecce149ad3a6fc32
yasminadaileh1 <yas.adaileh1@gmail.com>
2020-02-05 14:48:04 +0200

    activat js

--- js/add-content.js
+++ js/add-content.js
@@ -1,32 +1,22 @@
-var today = new Date();
-var hourNow = today.getHours();
-var greeting;
+alert ("Hello!")
 
-if (hourNow > 18 ) {
-    greeting = 'good evening!';
-} else if (hourNow > 12){
-    greeting ='good afternoon!';
-} else if (hourNow > 0) {
-    greeting ='good morning!';
-} else { 
-    greeting ='welcome';
+confirm ("do you like my work?")
+
+function greeting(){
+    var hourNow =prompt("how old are you?");
+    var greeting
+    if (hourNow < 20) {
+        greeting ='hello young!'
+    }
+    if (hourNow>21 && hourNow <50){
+        greeting ='hello youth'
+    }
+    if (hourNow > 51) {
+        greeting ='hello eldery'
+    }
+    
+    // console.log (greeting);
+    
+    return greeting;
 }
-
-document.write('<h3>' + greeting + '</h3>');
-
-
-//alert ("Hello!")
-
-//confirm ("do you like my work?")
-
-var hourNow =prompt("how old are you?");
-var greeting
-if (hourNow < 20) {
-    greeting ='hello young!'
-}
-if (hourNow>21 && hourNow <50){
-    greeting ='hello youth'
-}
-if (hourNow > 51) {
-    greeting ='hello eldery'
-}
+greeting();
